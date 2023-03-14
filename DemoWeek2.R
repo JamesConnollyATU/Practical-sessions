9682cae065c5b0ddb00e40d6c0115993a2fa8f31
Connolly James <james.connolly@atu.ie>
2023-03-14 15:30:13 +0000

    initial commit

---
+++ DemoWeek2.R
@@ -0,0 +1,36 @@
+a<-b<-6
+rm(a)
+a
+class(b)
+char1<- "hello"
+class(char1)
+is.numeric(b)
+is.numeric(char1)
+
+
+i <- 5L
+is.numeric(i)
+is.integer(i)
+
+i <- 9.789L
+i
+
+a <- 4L
+b <- 3L
+d <- a/b
+class (d)
+
+a <- 4L
+d <- a*.3
+class(d)
+
+x <- "data"
+x
+y <-factor("data", levels = c("data", "data1"))
+
+gender <- factor("male",levels = c("male", "female"))
+
+
+
+
+
