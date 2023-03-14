ab6bccbfe1582cc8611d56722c82e711523fe79f
Connolly James <james.connolly@atu.ie>
2023-03-14 14:24:13 +0000

    initial commit

---
+++ practical14032023.R
@@ -0,0 +1,30 @@
+# vector x = 3, 12, 7
+x <- c(3, 12, 7)
+y <- c(4, 9, 11)
+
+z <- x + y
+z
+
+x <- x + 2
+y <- y + 50
+
+x
+y
+
+x <- sum(x)
+y <- mean(y)
+x
+w <- c(2, 7, NA, 20)
+v <- sum(w, na.rm = TRUE)
+v
+
+x > y
+
+x <- append(x, 12)
+x
+
+u <- c(5:20)
+u <- append(u, letters[6:10])
+u
+
+
